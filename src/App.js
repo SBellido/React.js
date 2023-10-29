5710fce5f67c35b359e1a630b6692e5aae1333a7
SBellido <sebastianbellidodg@gmail.com>
2023-10-29 11:42:12 +0100

    Using props in TodoList

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreatedTodoButton';
 import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Superar challenge', completed: true },
+  { text: 'Llamar a la familia', completed: true },
+  { text: 'Comprar pan y vino', completed: false },
+  { text: 'Volver a España', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-
+    <React.Fragment>
       <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButton />
-
-    </div>
+    </React.Fragment>
   );
 }
 
