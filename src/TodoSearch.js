e4f5e94ea426a039a7210106f5db956467681e53
SBellido <sebastianbellidodg@gmail.com>
2023-10-30 11:12:26 +0100

    Event onchange was added

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,16 @@ import './TodoSearch.css';
 
 function TodoSearch() {
   return (
-    <input placeholder="Escriba la tarea a realizar" 
-    className="TodoSearch" /> 
+    <input 
+      placeholder="Escriba la tarea a realizar" 
+      className="TodoSearch" 
+      onChange={ (event) =>{
+        console.log('Escribiste en el TODO Search')
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    /> 
   );
 }
 
