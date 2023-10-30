dd869b4d4892326d2cf7f4ebf86d8306c5ad21a2
SBellido <sebastianbellidodg@gmail.com>
2023-10-30 12:42:00 +0100

    Added derived states in the SearchTodo Component

--- src/App.js
+++ src/App.js
@@ -13,13 +13,22 @@ const defaultTodos = [
 ];
 
 function App() {
+  // ESTADOS
   const [todos, setTodos] = React.useState(defaultTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
+ // ESTADOS DERIVADOS
   const completedTodos = todos.filter(
     todo => !!todo.completed).length;
-    
   const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
 
   console.log('Los usuarios buscan TODOS de ' + searchValue);
 
@@ -32,7 +41,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
             key={todo.text} 
             text={todo.text}
